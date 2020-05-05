62381e260cf860b237c217a65ac8565e3d7266eb
jOh <j0h@example.com>
2020-05-04 23:55:38 -0300

    Updates

---
+++ precache-manifest.bcc4c4907a9adadfe8c7eb6e2b706339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821d81181b42af7b0305515b0a50652a",
+    "url": "/App-del-Clima/index.html"
+  },
+  {
+    "revision": "13dfc888208dbdf07e72",
+    "url": "/App-del-Clima/static/css/main.a9323fb4.chunk.css"
+  },
+  {
+    "revision": "d9f4af990a29fce56b8c",
+    "url": "/App-del-Clima/static/js/2.d9d99ad3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/App-del-Clima/static/js/2.d9d99ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13dfc888208dbdf07e72",
+    "url": "/App-del-Clima/static/js/main.136af277.chunk.js"
+  },
+  {
+    "revision": "9af27ac9e22713dba016",
+    "url": "/App-del-Clima/static/js/runtime-main.dfc117bd.js"
+  }
+]);
